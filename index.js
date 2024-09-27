4e9b366d0c84e1e34e4dc3f3f765c29a99d1696a
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-27 15:17:35 -0300

    feat(t1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
-const port = process.env.PORT || 3000;
-const url = process.env.URL;
-const apiKey = process.env.API_KEY;
+const envs = require("./envs");
+
+express.json();
+app.use(cookieParser());
 
 // Exercício 1
-app.get("/coord", async (req, res) => {
-  const city = req.query.city;
-  const response = await fetch(
-    `${url}/geo/1.0/direct?q=${city}&appid=${apiKey}`
-  );
-  const data = await response.json();
-  res.json({
-    latitude: data[0].lat,
-    longitude: data[0].lon,
-  })
+app.get("/coords", async (req, res) => {
+  try {
+    const city = req.query.city;
+    const response = await fetch(
+      `${envs.url}/geo/1.0/direct?q=${city}&appid=${envs.apiKey}`
+    );
+  
+    const data = await response.json();
+  
+    const lat = data[0].lat;
+    const lon = data[0].lon;
+  
+    res.json({
+      latitude: lat,
+      longitude: lon,
+    })
+
+  } catch (error) {
+    res.status(500).send({ error: "Erro ao buscar coordenadas." });
+  }
 });
 
-app.listen(port, () => {
-    console.log(`Servidor iniciado. Porta: ${port}`);
+// Exercício 2
+app.get("/weather", async (req, res) => {
+  try {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+
+    // /weather?lon=xxxx&lat=yyyy
+
+    const response = await fetch(
+      `${envs.url}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${envs.apiKey}`
+    );
+  
+    const data = await response.json();
+  
+    const feelsLike = data.main.feels_like;
+    const description = data.weather[0].description;
+  
+    res.json({
+      sensacao_termica: feelsLike,
+      descricao: description,
     });
+
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar previsão do tempo." });
+  }  
+})
+
+app.listen(envs.port, () => {
+  console.log(`Servidor iniciado. Porta: ${envs.port}`);
+});
